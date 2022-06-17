b14ee600295a9ef9e6d3760daef0462fb2c29a1d
sedaliSedalian <seyedalian@outlook.com>
2022-06-17 16:02:42 +0430

    modified author and created date

--- backend/src/posts/api/serializers.py
+++ backend/src/posts/api/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from ..models import Post
 
 class PostModelSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+    created = serializers.SerializerMethodField()
     class Meta:
         model = Post
         fields =  ['id','title','description','created','author']
+    
+    def get_author(self, obj):
+        return obj.author.username
+    def get_created(self, obj):
+        return obj.created.strftime('%d-%m-%Y, %H:%M:%S')
